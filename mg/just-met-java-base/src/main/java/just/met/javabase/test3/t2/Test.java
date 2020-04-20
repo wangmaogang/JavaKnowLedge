00ca5a1e477bd5c540159ab360647425656404df
wangmaogang <986587155@qq.com>
2020-04-20 16:38:57 +0800

    Java反射学习

--- mg/just-met-java-base/src/main/java/just/met/javabase/test3/t2/Test.java
+++ mg/just-met-java-base/src/main/java/just/met/javabase/test3/t2/Test.java
@@ -1,14 +1,5 @@
 package just.met.javabase.test3.t2;
 
-import just.met.javabase.test3.RefectionPractice;
-import just.met.javabase.test3.t1.DeviceFactory;
-import just.met.javabase.test3.t1.IComputer;
-import just.met.javabase.test3.t1.IPhone;
-import just.met.javabase.test3.t1.impl.HuaWeiComputer;
-import just.met.javabase.test3.t1.impl.HuaWeiPhone;
-import just.met.javabase.test3.t1.impl.MiComputer;
-import just.met.javabase.test3.t1.impl.MiPhone;
-
 /**
  * @author wangmaogang
  */
